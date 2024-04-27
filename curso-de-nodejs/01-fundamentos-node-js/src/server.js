998849a77428387c29a67734955569d65d57a781
ImGabreuw <imgabreuw.dev@gmail.com>
2024-04-26 23:47:21 -0300

    Estrutura da aplicação

--- curso-de-nodejs/01-fundamentos-node-js/src/server.js
+++ curso-de-nodejs/01-fundamentos-node-js/src/server.js
@@ -1,7 +1,26 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
-    return response.end('Hello World!')
+    const { method, url } = request
+    
+    if (method === 'GET' && url === '/users') {
+        return response
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
+        return response.writeHead(201).end('Criação de usuário')
+    }
+
+    return response.writeHead(404).end('Not found')
 })
 
 server.listen(3000)
